36f4521fe1320787b3f6c3778ec5f9c7b430ddd2
zaicev-maxim <zaiceffmaksim@gmail.com>
2020-10-29 09:29:09 +0300

    changed traccar configuration for deploying

--- src/main/java/org/traccar/web/WebServer.java
+++ src/main/java/org/traccar/web/WebServer.java
@@ -114,8 +114,8 @@ public class WebServer {
     }
 
     private void initClientProxy(Config config, HandlerList handlers) {
-        int port = config.getInteger("osmand.port");
-//        int port = config.getInteger("watch.port");
+//        int port = config.getInteger("osmand.port");
+        int port = config.getInteger("watch.port");
         if (port != 0) {
             System.out.println(port);
             ServletContextHandler servletHandler = new ServletContextHandler() {
